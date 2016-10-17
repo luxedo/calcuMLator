7ab75a49279e1cabdde38dd01ee38ea4c0780090
Luiz Eduardo Amaral <luizamaral306@gmail.com>
2016-10-17 16:40:38 -0200

    Added redirection

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 def app(environ, start_response):
     """Simplest possible application object"""
-    data = 'Hello, World!\n'
-    status = '200 OK'
-    response_headers = [
-        ('Content-type','text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
+    print  environ['PATH_INFO']
+    data = '<h1>Hello, World!</h1>\n'
+
+    if environ['PATH_INFO'] is not "/":
+        status = '302 Found'
+        response_headers = [('Location','/')]
+    else:
+        status = '200 OK'
+        response_headers = [
+            ('Content-type','text/html'),
+            ('Content-Length', str(len(data)))
+        ]
     start_response(status, response_headers)
     return iter([data])
